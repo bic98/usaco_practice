bd21a674bf484efab04387454d8a54b5c1a83c04
inchanBaek <bic7885@gmail.com>
2025-05-11 11:18:37 +0900

    .

---
+++ 27839.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+int main(){
+	fastio; 
+	int n; cin >> n; 
+	cout << fixed << setprecision(2);
+	double ans = n / 4.0; 
+	cout << ans << '\n'; 
+}
